6456bf253072682683770cf3d194a3fb2f8e86a6
nbyrd9 <nianibyrd@gmail.com>
2021-01-27 13:06:43 -0500

    commit tree 1805360c00dadc90c41fd810f41ebcb58562a54d

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".site-header__hero__headline") 
+
+doc.css(".tout__label.heading.heading--level-4")
+p doc.css(".tout__label.heading.heading--level-4")[0].name
